081b28fd8c06fb092e29ee0630278f3ae0f5c70e
johargogo <gogodatujohar@gmail.com>
2025-08-26 21:37:37 +0800

    Added Bootstrap template and Home controller

--- app/Views/home.php
+++
@@ -1,7 +0,0 @@
-<?= $this->extend('template') ?>
-
-<?= $this->section('content') ?>
-    <div class="alert alert-primary">
-        <h3>Welcome to ITE311-DORAIDO App Homepageses!</h3>
-    </div>
-<?= $this->endSection() ?>
