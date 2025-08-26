f94b005e1be1f594e1ced43516d11d2fa321ff8d
johargogo <gogodatujohar@gmail.com>
2025-08-26 21:43:27 +0800

    Merge remote changes into local

---
+++ app/Views/home.php
@@ -0,0 +1,7 @@
+<?= $this->extend('template') ?>
+
+<?= $this->section('content') ?>
+    <div class="alert alert-primary">
+        <h3>Welcome to ITE311-DORAIDO App Homepageses!</h3>
+    </div>
+<?= $this->endSection() ?>
